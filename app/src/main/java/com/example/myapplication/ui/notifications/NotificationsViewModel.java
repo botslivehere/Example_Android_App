013e12b9551daad738eddbe1c472a6d6c1b534fa
Artur <avdzhyan2033@mail.ru>
2023-10-19 20:06:09 +0300

    fixes 2

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsViewModel.java
@@ -10,7 +10,7 @@ public class NotificationsViewModel extends ViewModel {
 
     public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("Ваше избранное:");
     }
 
     public LiveData<String> getText() {
